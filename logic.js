184f4ede9cf1e154c5fa72583db4038eb479f0e7
Laboratorio 307 <estudiante307@unicamacho.co>
2024-10-03 08:19:15 -0500

    numeros Naturales

---
+++ logic.js
@@ -0,0 +1,11 @@
+var numeroEntrada= 0;
+var resultado=0;
+var control=0;
+numeroEntrada= parseFloat(prompt("digitame el numero que quieres procesar"));
+while(numeroEntrada>=control) {
+    resultado=resultado+control;
+    control=control+1;
+}
+
+alert("la suma de los primeros "+numeroEntrada+" naturales es "+resultado);
+
